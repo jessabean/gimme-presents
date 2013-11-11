e78e0d477ba1fb5886725cda3cb32858e7b9df22
Jessica Eldredge <jessica.avison@gmail.com>
2013-11-11 18:58:30 -0500

    Generate app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GimmePresents::Application.config.secret_key_base = '3b6094416908755d1cee595392fea40cfe3fd4c7c7808a3a544cbac8546d82439f2781bc0e0ce766e385b628851827b5ed1dfd543bd7d2a60f4bbba0578bb458'
